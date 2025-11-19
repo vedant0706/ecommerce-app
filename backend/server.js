fc5fc5a8fc025300be7ccefc94d5f2db31c5692e
Vedant Jadhav <vedantjadhav173@gmail.com>
2025-11-19 23:58:29 +0530

    Initial backend commit

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectCloudinary();
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://aura-ecommerce-sandy.vercel.app","https://aura-ecommerce-admin.vercel.app"],
     credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ["Content-Type", "Authorization", "token"],
