b37a8ac44147d80cb5be0757f0274cf67614d848
Vedant Jadhav <vedantjadhav173@gmail.com>
2025-11-21 17:22:54 +0530

    changes on Login

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ connectCloudinary();
 app.use(
   cors({
     origin: [
-      // "http://localhost:5173",
-      // "http://localhost:5174",
+      "http://localhost:5173",
+      "http://localhost:5174",
       "https://aura-ecommerce-app.vercel.app",
-      // "https://admin-aura-ecommerce-app.vercel.app",
+      "https://admin-aura-ecommerce-app.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
